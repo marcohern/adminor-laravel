290d9d81e94ca8bd3098f5a40c5b201835fc2675
Marco Hernandez <marcohern@gmail.com>
2023-04-23 11:53:50 -0500

    single list function

--- app/Daos/StateDao.php
+++ app/Daos/StateDao.php
@@ -5,6 +5,7 @@ namespace App\Daos;
 use App\Models\State;
 use App\Models\Country;
 use Illuminate\Database\Eloquent\Collection;
+use stdClass;
 
 class StateDao {
 
@@ -13,16 +14,38 @@ class StateDao {
 
   }
 
-  private function listByCountryCode($code) : Collection
+  public function list($source) : Collection
   {
-    $country = $this->countryDao->find($code);
+    switch(gettype($source)) {
+      case "object":
+        return $this->listByObject($source);
+      case "integer":
+      case "double":
+      case "string":
+      default:
+        return $this->listByScalar($source);
+    }
+  }
+
+  private function listByScalar($source)
+  {
+    $country = $this->countryDao->find($source);
     return State::where('country_id',$country->id)->list();
+  }
+
+  private function listByObject($source)
+  {
+    if (is_a($source, Country::class)) {
+      return State::where('country_id',$source->id)->list();
+    }
+    return collect();
   }
 
   public function find($source) : ?State
   {
     switch(gettype($source)) {
       case "integer":
+      case "double":
         return $this->findById($source);
       default:
         return $this->findByIso3($source);
